66af8f534aa72b93a225f5f1fbfdf1213bd93f3a
Enmanuel De los Santos Cruz <41967722+Eldor48@users.noreply.github.com>
2022-05-28 21:25:47 -0400

    finishing the game

---
+++ jumper/game/word.py
@@ -0,0 +1,11 @@
+import random  
+list = ['apple', 'banana', 'orange', 'pear','pineapple', 'strawberry', 'watermelon','dragonfruit', 'grapefruit', 'kiwi', 'lemon', 'lime', 'mango', 'melon', 'nectarine', 'peach', 'plum', 'pomegranate', 'raspberry', 'tangerine', 'tomato', 'watermelon', 'watermelon', 'audi', 'toyota',"haphazard", "hyphen", "iatrogenic", "icebox", "jackpot", "jaundice", "jawbreaker",
+"jaywalk", "jazziest", "kayak", "kazoo", "keyhole", "khaki", "kilobyte", "larynx", "lengths", "lucky",
+"luxury", "lymph", "marquis", "matrix", "megahertz", "microwave", "mnemonic", "naphtha", "nightclub",
+"nowadays", "numbskull", "nymph", "onyx", "ovary", "oxidize", "oxygen", "pajama", "peekaboo", "phlegm",
+"pixel", "pizazz", "quartz", "queue", "quips", "quixotic", "quiz", "razzmatazz", "rhubarb", "rhythm",
+"rickshaw", "schnapps", "scratch", "shiv", "snazzy", "sphinx", "thriftless", "thumbscrew", "topaz",
+"transcript", "transgress", "unknown", "unworthy", "unzip", "uptown", "vaporize", "vixen", "vodka",
+"voodoo", "vortex", "walkway", "waltz", "wave", "wavy", "wyvern", "xylophone", "yachtsman", "yippee", "yoked",
+"youthful", "yummy", "zephyr", "zigzag", "zigzagging", "zilch", "zombie"]
+words = random.choice(list).upper()
