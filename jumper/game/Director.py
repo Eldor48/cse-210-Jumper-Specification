0a8a0dd1a7797d168732cf15a6904fd681029d4d
Enmanuel De los Santos Cruz <41967722+Eldor48@users.noreply.github.com>
2022-05-28 20:45:07 -0400

    finishing director

--- jumper/game/Director.py
+++ jumper/game/Director.py
@@ -6,9 +6,22 @@ from game.console import Console
 
 class Director:
         def __init__(self):
-                self.jumper = Jumper()
-                self.console = Console()
-                self.console.set_jumper(self.jumper)
-                self.console.set_words(words)
-                self.console.set_director(self)
-                self.console.start_game()
+            self.keep_playing = True
+            self.console = Console()
+            self.jump = Jumper()
+            self.word  = words()
+        
+        def star_game(self):
+            
+            while self.keep_playing:
+                self.get_inputs()
+                self.do_updates()
+                self.do_outputs()
+        
+        def get_inputs(self):
+            self.word
+        def do_updates(self):
+            self.jump.process()
+            
+        def do_outputs(self):
+            pass
